ad931bef69f31520c7aa0ebfb2af483da48e33d5
Shake Gharibyan <Shake_Gharibyan@epam.com>
2021-04-01 03:00:31 +0400

    Uptade locators, add

--- src/test/java/SixPMTest.java
+++ src/test/java/SixPMTest.java
@@ -5,17 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
 public class SixPMTest {
 
-    private WebDriver driver = null;
+    private WebDriver driver;
+    private Actions actions;
+    private WebDriverWait wait;
+    private WebElement removedItem;
+    String actualPrice;
 
     @BeforeSuite
     private void setSystemProperties() {
@@ -25,24 +26,14 @@ public class SixPMTest {
     @BeforeMethod
     private void createDriverInstance() {
         driver = new ChromeDriver();
-    }
-
-    @AfterMethod
-    private void tearDownDriverInstance() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     private void test6PMMainFunctionalities() throws InterruptedException {
         driver.get("https://www.6pm.com/");
-        driver.manage().window().maximize() ;
-
-        Actions actions = new Actions(driver);
-
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.manage().window().maximize();
 
         By accessoriesButtonLoc = By.xpath("//*[text()=\"Accessories\"]");
         WebElement accessoriesButton = driver.findElement(accessoriesButtonLoc);
@@ -56,47 +47,56 @@ public class SixPMTest {
         actions.moveToElement(aviatorButton).click().build().perform();
 
 
-        By searchPageLoc = By.xpath("//*[@class='py-z ns-z']");
+        By searchPageLoc = By.xpath("//*[@itemtype='http://schema.org/Product']");
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(searchPageLoc));
 
-        List<WebElement> aviators = driver.findElements(By.xpath("//*[@class='py-z ns-z']"));
+        List<WebElement> aviators = driver.findElements(searchPageLoc);
         WebElement aviator = Utils.randomItem(aviators);
 
         actions.moveToElement(aviator).click().build().perform();
 
-        By itemPageLoc = By.xpath("//*[@class='Gb-z']");
+        By itemPageLoc = By.xpath("//*[@class='Rk-z']");
         wait.until(ExpectedConditions.elementToBeClickable(itemPageLoc));
 
-        String expectedName = driver.findElement(By.xpath("//*[@class='fN-z']")).getAttribute("innerHTML");
-        String expectedPrice = driver.findElement(By.xpath("//*[@class='tK-z yK-z']")).getAttribute("aria-label");
+        String expectedName = driver.findElement(By.xpath("//*[@itemprop='name' and @class='Sw-z']")).getAttribute("innerHTML");
+        String expectedPrice = driver.findElement(By.xpath("//*[@class='Ao-z Fo-z']")).getAttribute("aria-label");
 
         WebElement addToShoppingBagButton = driver.findElement(By.xpath("//button[text()='Add to Shopping Bag']"));
-        actions.moveToElement(addToShoppingBagButton).click().build().perform();
+        addToShoppingBagButton.click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='oz-z']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='BM-z']")));
 
-        WebElement viewBagButton = driver.findElement(By.xpath("//*[@class='sz-z']"));
+        WebElement viewBagButton = driver.findElement(By.xpath("//*[@class='FM-z']"));
         actions.moveToElement(viewBagButton).click().build().perform();
 
-        By itemInBagContainerLoc = By.xpath("//*[@class='pc-z']");
+        By itemInBagContainerLoc = By.xpath("//*[@class='Uq-z']");
         wait.until(ExpectedConditions.visibilityOfElementLocated(itemInBagContainerLoc));
 
-        String actualName = driver.findElement(By.xpath("//div[@class='Wk-z']/a")).getText().replace('\n', ' ');
+        String actualName = driver.findElement(By.xpath("//div[@class='ft-z']/a")).getText().replace('\n', ' ');
 
-        String actualPrice = driver.findElement(By.xpath("//*[@class='yh-z Bh-z']")).getText();
+        actualPrice = driver.findElement(By.xpath("//em")).getText();
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(actualName, expectedName, "Name of the item doesn't match the expected value.");
         softAssert.assertEquals(actualPrice, expectedPrice, "Price of the item doesn't match the expected value.");
         softAssert.assertAll();
+    }
 
-        WebElement quantityButton = driver.findElement(By.name("quantity"));
+    @AfterMethod
+    private void tearDownDriverInstance() throws Exception {
+        removedItem = driver.findElement(By.xpath(String.format("//*[contains(text(), '%s')]", actualPrice)));
+
+        WebElement quantityButton = driver.findElement(By.xpath("//*[@name='quantity']"));
         actions.moveToElement(quantityButton).click().build().perform();
 
         WebElement removeOption = driver.findElement(By.xpath("//*[text()='Remove']"));
         removeOption.click();
 
-        String textCheck = "Nothing to see here yet! Sign in to see items that you've previously placed in your Bag or check out all the awesome things you can buy on 6pm!";
-        wait.until(ExpectedConditions.textToBe(By.xpath("//h1/following-sibling::p"), textCheck));
+        wait.until(ExpectedConditions.invisibilityOf(removedItem));
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
