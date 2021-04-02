306fe0500a8552e7498892574a06b00d68ad1a4b
Shake Gharibyan <Shake_Gharibyan@epam.com>
2021-04-02 19:39:25 +0400

    Apply PageFactory to src/main/java/amazon/AuthorPage.java, src/main/java/amazon/AuthorSearchPage.java, src/main/java/amazon/HomePage.java.

--- src/main/java/amazon/AuthorPage.java
+++ src/main/java/amazon/AuthorPage.java
@@ -3,49 +3,65 @@ package amazon;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class AuthorPage {
 
-    private WebDriver driver;
+    private final WebDriver driver;
 
-    private By booksBySectionLoc = By.id("booksBySection");
-    private By priceOfBookDivLoc = By.xpath("//*[@class='a-fixed-left-grid a-spacing-small']");
-    private By priceOfBookLoc = By.xpath("//span[@class='a-size-base-plus a-color-price a-text-bold']/span");
-    private By sortBySelectorsLoc = By.id("sortBySelectors");
-    private By sortByPriceLowToHighButtonLoc = By.xpath("//a[contains(text(),'Price: Low to High')]");
+    private static final String booksBySectionIDContent = "booksBySection";
+    @FindBy(id = booksBySectionIDContent)
+    WebElement booksByAuthor;
+
+    private static final String priceOfBookDivXpathContent = "//*[@class='a-fixed-left-grid a-spacing-small']";
+    @FindBy(xpath = priceOfBookDivXpathContent)
+    WebElement priceOfBookDiv;
+
+    private static final String pricesOfBookXpathContent = "//span[@class='a-size-base-plus a-color-price a-text-bold']/span";
+    @FindBys({
+            @FindBy(xpath = pricesOfBookXpathContent)
+    })
+    List<WebElement> pricesOfBooks;
+
+    private static final String sortBySelectorsIDContent = "sortBySelectors";
+    @FindBy(id = sortBySelectorsIDContent)
+    WebElement sortBySelectorsButton;
+
+    private static final String sortByPriceLowToHighButtonXpathContent = "//a[contains(text(),'Price: Low to High')]";
+    @FindBy(xpath = sortByPriceLowToHighButtonXpathContent)
+    WebElement sortByPriceLowToHighButton;
 
 
     public AuthorPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void waitUntilPageLoads() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(priceOfBookDivLoc));
-        wait.until(ExpectedConditions.numberOfElementsToBe(priceOfBookDivLoc, 12));
+        wait.until(ExpectedConditions.visibilityOfAllElements(priceOfBookDiv));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath(priceOfBookDivXpathContent), 12));
     }
 
     public String booksByAuthorText() {
-        WebElement booksByAuthor = driver.findElement(booksBySectionLoc);
         return booksByAuthor.getText().toLowerCase();
     }
 
     public void sortByPriceLowToHigh() {
-        WebElement sortBySelectorsButton = driver.findElement(sortBySelectorsLoc);
+
         sortBySelectorsButton.click();
 
-        WebElement sortByPriceLowToHighButton = driver.findElement(sortByPriceLowToHighButtonLoc);
         sortByPriceLowToHighButton.click();
     }
 
     public List<Double> getPricesOfBooks() {
-        List<WebElement> pricesOfBooks = driver.findElements(priceOfBookLoc);
         return pricesOfBooks.stream()
                 .map(priceStr -> parseStringToDouble(priceStr.getText()))
                 .collect(Collectors.toList());
